e2639d6cc67fb6e7223ed55536153167417941a2
Schulz Hauptmann <100137249+zabit923@users.noreply.github.com>
2023-07-22 21:17:14 +0300

    Add models for blog

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,31 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
 
 
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    parent = TreeForeignKey(
+        'self',
+        related_name='children',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    class MPTTMeta:
+        level_attr = 'mptt_level'
+        order_insertion_by=['name']
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
