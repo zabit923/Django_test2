755d9d1aa30ec6ca57210e8f4ea3bbbb4370d689
Schulz Hauptmann <100137249+zabit923@users.noreply.github.com>
2023-07-25 20:06:28 +0300

    Update models for blog +Recipe, +Comment, made migrations, added models to admin

--- blog/models.py
+++ blog/models.py
@@ -30,6 +30,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to='articles/')
     text = models.TextField()
+
     category = models.ForeignKey(
         Category,
         related_name='post',
@@ -39,3 +40,32 @@ class Post(models.Model):
     tags = models.ManyToManyField(Tag, related_name='post')
     create_at = models.DateTimeField(auto_now_add=True)
 
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    serves = models.CharField(max_length=50)
+    prep_time = models.PositiveIntegerField(default=0)
+    cook_time = models.PositiveIntegerField(default=0)
+    ingredients = models.TextField()
+    direction = models.TextField()
+
+    post = models.ForeignKey(
+        Post,
+        related_name='recipe',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    website = models.CharField(max_length=150)
+    massage = models.TextField(max_length=500)
+
+    post = models.ForeignKey(
+        Post,
+        related_name='comment',
+        on_delete=models.CASCADE,
+    )
