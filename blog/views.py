18b5319ed7b4ee16f1b31a61f8aae49fcdeb2468
Schulz Hauptmann <100137249+zabit923@users.noreply.github.com>
2023-07-30 19:42:39 +0300

    + Views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Post
+
+
+
+class PostListView(ListView):
+    model = Post
+
+
+class HomeView(TemplateView):
+    template_name = 'index.html'
--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from django.urls import include
+
+from cook.blog.views import HomeView
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='index'),
 ]
 
 if settings.DEBUG:
